be3dd55d11aa5e4ce96d6295fa90afd63a9f4117
Illia Shakin <S00188372@mail.itsligo.ie>
2021-05-11 09:53:46 +0100

    Completed Nunit Testing With Passing result

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using IlliaShakin_S00188372;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            Game G1 = new Game("Miles Morales",85,"","PS",100m,"");
+            decimal fPrice = 90m;
+
+            //Act
+            G1.DescreasePrice(.10);
+
+            //Assert
+            Assert.AreEqual(fPrice, G1.Price);
+
+        }
+    }
+}
